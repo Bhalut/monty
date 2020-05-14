3772913f63237d7bf90f6ee1a461da04116d8bfb
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-14 13:34:17 -0500

    fix main variables

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "monty.h"
-hero savior;
+hero_t savior;
 /**
  *  main -  interpreter for Monty ByteCodes files
  *  @argc: Number of paramethers
@@ -10,12 +10,12 @@ hero savior;
 int main(int argc, char **argv)
 {
 	/* Open the file for reading */
-	char *line_buf = NULL, *file_name = argv[1];
+	char *file_name = argv[1];
 	size_t line_buf_size = 0;
 	ssize_t line_size;
 	unsigned int line_count = 0;
-	FILE *fp;
-	stack_t *main_stack = NULL;
+	FILE *fp = NULL;
+	stack_t *main_stack;
 
 	main_stack = NULL;
 	savior.stack_head = main_stack;
@@ -30,26 +30,26 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error opening file '%s'\n", file_name);
 		exit(EXIT_FAILURE);
 	}
+	savior.fp_struct = fp;
 	/* Get the first line of the file. */
-	line_size = getline(&line_buf, &line_buf_size, fp);
-	savior.getl_info = line_buf;
+	line_size = getline(&savior.getl_info, &line_buf_size, fp);
 	/* Loop through until we are done with the file. */
 	while (line_size >= 0)
 	{
 		/* Increment our line count */
 		line_count++;
 		/* Function Call to identity the op */
-		opcode(line_buf, &main_stack, line_count);
+		opcode(savior.getl_info, &savior.stack_head, line_count);
+
 		/* Show the line details */
 		/* printf("line[%06d]: contents: %s", line_count, line_buf); */
 		/* Get the next line */
-		line_size = getline(&line_buf, &line_buf_size, fp);
-		savior.getl_info = line_buf;
+		line_size = getline(&savior.getl_info, &line_buf_size, fp);
 	}
 	/* Free the allocated line buffer */
-	free(line_buf);
-	line_buf = NULL;
-	free_grid(savior.grid);
+	free(savior.getl_info);
+	free(savior.command_struct);
+	savior.getl_info = NULL;
 	free_dlistint(savior.stack_head);
 	/* Close the file now that we are done with it */
 	fclose(fp);
