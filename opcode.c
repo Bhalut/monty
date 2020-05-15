1755d871eaf531b8db189f4061b665832771cc4f
nicolasquinchia <nicolasquinchia@gmail.com>
2020-05-14 22:16:07 -0500

    add functions to structures

--- opcode.c
+++ opcode.c
@@ -13,6 +13,10 @@ void opcode(char *command)
 	    {"push", op_push},
 	    {"pall", op_pall},
 	    {"pint", op_pint},
+	    {"pop", op_pop},
+	    {"swap", op_swap},
+	    {"add", op_add},
+	    {"nop", op_nop},
 	    {NULL, NULL},
 	};
 
